e81e8441b03cba188c3630728b71b8772eb55fc0
otemALS <mateo11102005@gmail.com>
2025-01-30 14:30:17 +0100

    ca marche mais pas trop

--- app/src/main/java/bts/sio/azurimmo1/views/BatimentList.kt
+++ app/src/main/java/bts/sio/azurimmo1/views/BatimentList.kt
@@ -2,6 +2,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -9,6 +10,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+
 
 @Composable
 fun BatimentList( viewModel: BatimentViewModel = viewModel()) {
@@ -22,6 +25,7 @@ fun BatimentList( viewModel: BatimentViewModel = viewModel()) {
                     modifier = Modifier.align(Alignment.Center)
                 )
             }
+
             errorMessage != null -> {
                 Text(
                     text = errorMessage ?: "Erreur inconnue",
@@ -31,6 +35,7 @@ fun BatimentList( viewModel: BatimentViewModel = viewModel()) {
                     color = MaterialTheme.colorScheme.error
                 )
             }
+
             else -> {
                 LazyColumn {
                     items(batiments) { batiment ->
@@ -40,3 +45,4 @@ fun BatimentList( viewModel: BatimentViewModel = viewModel()) {
             }
         }
     }
+}

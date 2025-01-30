5aab4f0b6998ba4b3cf433813778aeee3c021617
otemALS <mateo11102005@gmail.com>
2025-01-30 14:49:54 +0100

    crittique

--- app/src/main/java/bts/sio/azurimmo1/views/AppartementList.kt
+++ app/src/main/java/bts/sio/azurimmo1/views/AppartementList.kt
@@ -1,4 +1,17 @@
-import bts.sio.azurimmo1.model.Appartement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+
 
 @Composable
 fun AppartementList( viewModel: AppartementViewModel = viewModel()) {
@@ -12,6 +25,7 @@ fun AppartementList( viewModel: AppartementViewModel = viewModel()) {
                     modifier = Modifier.align(Alignment.Center)
                 )
             }
+
             errorMessage != null -> {
                 Text(
                     text = errorMessage ?: "Erreur inconnue",
@@ -21,12 +35,14 @@ fun AppartementList( viewModel: AppartementViewModel = viewModel()) {
                     color = MaterialTheme.colorScheme.error
                 )
             }
+
             else -> {
                 LazyColumn {
-                    items(batiments) { batiment ->
-                        BatimentCard(batiment = batiment) // Appel de la fonction BatimentCard
+                    items(appartements) { appartement ->
+                        AppartementCard(appartement = appartement) // Appel de la fonction BatimentCard
                     }
                 }
             }
         }
     }
+}

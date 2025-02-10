8ed8a779fd069639e97f4ef81004373e4d96fed7
otemALS <mateo11102005@gmail.com>
2025-02-10 09:47:50 +0100

    fed

--- app/src/main/java/bts/sio/azurimmo1/api/RetrofitInstance.kt
+++ app/src/main/java/bts/sio/azurimmo1/api/RetrofitInstance.kt
@@ -5,14 +5,18 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 
 class RetrofitInstance {
-    private const val BASE_URL = "http://10.0.2.2:9008/"
-    val api: ApiService by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(ApiService::class.java)
+    companion object {
+        private const val BASE_URL = "http://10.0.2.2:9008/"
+
+        val api: ApiService by lazy {
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(ApiService::class.java)
+        }
     }
 }
 
-}
+
+
